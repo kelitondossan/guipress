be8a7d6d1ac3c3491e4734ffed8d6e5dbac017e0
keliton santos <71714438+kelitondossan@users.noreply.github.com>
2023-08-06 17:34:31 -0300

    update

--- categorias/categoriasControler.js
+++ categorias/categoriasControler.js
@@ -29,9 +29,54 @@ router.get("/admin/categorias/new", (req, res) => {
      categoria.findAll().then(categorias =>{
         res.render("admin/categorias/index", {categorias: categorias});
      });
-
-  
    });
+
+   router.post("/categorias/delete", (req,res) =>{
+      var id = req.body.id;
+  //verificando de der algun resultado indefinido       
+      if(id != undefined){
+         if(!isNaN(id)){
+           
+            //função delete
+            categoria.destroy({
+               where: {
+                  id: id
+               }
+            }).then(() =>{
+               res.redirect("/admin/categorias");
+            })
+
+
+         }else{ /// se nao  for numerico
+            res.redirect("/admin/categorias");
+         }
+      }else{ // se for null
+         res.redirect("/admin/categorias");
+      }
+   });
+
+
+  router.get("/admin/categorias/edit/:id", (req, res) =>{
+   var id = req.params.id;
+    
+   if(isNaN(id)){
+      res.redirect("/admin/categorias");
+   }
+
+    categoria.findByPk(id).then(categoria => {
+      if(categoria != undefined){
+           
+         res.render("admin/categorias/edit",{categoria: categoria});
+
+      }else{
+               // Use o comando return após enviar a resposta para evitar o erro.
+        return   res.redirect("/admin/categorias");
+      }
+    }).catch(erro => {
+          // Use o comando return após enviar a resposta para evitar o erro.
+       return res.redirect("/admin/categorias");
+    })
+  })
 
 
 module.exports = router;
